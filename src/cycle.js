d3d8d783259a79c8ec2d110e095543ca2d76c0b6
Thaomai Nguyen <thaomaicnguyen@gmail.com>
2022-05-02 14:20:41 -0700

    submitted

--- src/cycle.js
+++ src/cycle.js
@@ -1,3 +1,18 @@
-const cycle = (list) => {};
+const cycle = (list) => {
+  // check for empty list
+  if (!list.head) return false;
+
+  let slowPointer = list.head;
+  let fastPointer = list.head;
+
+  // basic condition for loop to exist
+  while (fastPointer.next && fastPointer.next.next) {
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+
+    if (slowPointer == fastPointer) return true;
+  }
+  return false;
+};
 
 module.exports = cycle;
